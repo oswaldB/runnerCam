507073571fbd173848528b410aeef1b8d28f3a4a
Oswald BERNARD <oswald.bernard@metidia.com>
2016-12-14 11:49:38 +0100

    add shopitall2

---
+++ shopitall2/offline.js
@@ -0,0 +1,13 @@
+﻿{
+	"version": 1481712344,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/startmenu-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/success-sheet0.png",
+		"adapter.js"
+	]
+}
